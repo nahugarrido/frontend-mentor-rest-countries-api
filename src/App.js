ea1011440753e443af0a2689d30bc92c61b07cf6
Nahuel Garrido <garridonahuel8@gmail.com>
2023-03-09 00:07:06 -0300

    dark mode

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Card from "./components/card";
+import "./App.css";
 
 function App() {
+  const [mode, setMode] = useState("dark");
+
+  const changeMode = () => {
+    if (mode === "dark") {
+      setMode("light");
+    } else {
+      setMode("dark");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`size App-${mode}`}>
+      <button onClick={changeMode}>Cambiar modo</button>
+      <Card />
     </div>
   );
 }
