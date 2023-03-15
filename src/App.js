457ceccd4d56f5c37219f76ed4e67839716775a6
Nahuel <68355186+nahugarrido@users.noreply.github.com>
2023-03-14 21:05:50 -0300

    detailsPage

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RootLayout from "./pages/Root";
 import ErrorPage from "./pages/Error";
 import HomePage from "./pages/Home";
+import DetailsPage from "./pages/Details";
 import "./App.css";
 
 const router = createBrowserRouter([
@@ -10,7 +11,7 @@ const router = createBrowserRouter([
     path: "/",
     element: <RootLayout />,
     errorElement: <ErrorPage />,
-    children: [{ path: "/", element: <HomePage /> }],
+    children: [{ path: "/", element: <DetailsPage /> }],
   },
 ]);
 
--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,8 +1,84 @@
 import React from "react";
 import classes from "./Details.module.css";
+import { FaArrowLeft } from "react-icons/fa";
 
-const DetailsPage = () => {
-  return <div></div>;
+const DetailsPage = (props) => {
+  const country = {
+    id: "COL",
+    flags: {
+      png: "https://flagcdn.com/w320/co.png",
+      svg: "https://flagcdn.com/co.svg",
+      alt: "The flag of Colombia is composed of three horizontal bands of yellow, blue and red, with the yellow band twice the height of the other two bands.",
+    },
+    name: "Colombia",
+    nativeName: {
+      spa: {
+        official: "República de Colombia",
+        common: "Colombia",
+      },
+    },
+    population: 50882884,
+    region: "Americas",
+    continents: "Americas",
+    capital: ["Bogotá"],
+    domain: [".co"],
+    currencies: {
+      COP: {
+        name: "Colombian peso",
+        symbol: "$",
+      },
+    },
+    languages: {
+      spa: "Spanish",
+    },
+    borders: ["BRA", "ECU", "PAN", "PER", "VEN"],
+  };
+  return (
+    <>
+      <div className={classes["container"]}>
+        <div className={classes["top-container"]}>
+          <button className={classes.back}>
+            <FaArrowLeft className={classes.icon} />
+            Back
+          </button>
+        </div>
+        <div className={classes["img-container"]}>
+          <img
+            className={classes["img-inside"]}
+            src={country.flags.png}
+            alt="country flag"
+          />
+        </div>
+        <div className={classes.content}>
+          <h2>{country.name}</h2>
+          <h5>
+            Native Name: <span>{country.nativeName.spa.official}</span>
+          </h5>
+          <h5>
+            Population: <span>{country.population}</span>
+          </h5>
+          <h5>
+            Region: <span>{country.continents}</span>
+          </h5>
+          <h5>
+            Sub Region: <span>{country.region}</span>
+          </h5>
+          <h5>
+            Capital: <span>{country.capital}</span>
+          </h5>
+          <h5>
+            Top Level Domain: <span>{country.domain}</span>
+          </h5>
+          <h5>
+            Currencies: <span>{country.currencies[0].name}</span>
+          </h5>
+          <h5>
+            Languages: <span>{country.languages[0].name}</span>
+          </h5>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default DetailsPage;
